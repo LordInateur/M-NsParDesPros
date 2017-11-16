a4b8e98019c333ced55658681a6218d624cda87a
Fournier <matthieu.fournier@etu.univ-nantes.fr>
2017-11-16 22:33:47 +0100

    plusieur page
    Attention, ici seul la première page est afficher, la deuxième s'affiche en cas de problème

--- mn-app/src/index.js
+++ mn-app/src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Acceuil from './Acceuil';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+class Index extends Component {
+
+  genPageElem(element) {
+  	return element
+  }
+
+  render() {
+    return (
+      this.genPageElem(<Acceuil />),
+      this.genPageElem(<App />)
+    );
+  }
+}
+
+
+
+ReactDOM.render(<Index />, document.getElementById('root'));
 registerServiceWorker();
