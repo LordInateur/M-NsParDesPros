61b676def63b8e23eed79164c8dab1db405ffebf
Killy <goureau.n.a@hotmail.fr>
2017-11-17 15:44:15 +0100

    Changer Header and App

--- mn-app/src/js/Acceuil.js
+++ mn-app/src/js/Acceuil.js
@@ -13,17 +13,10 @@ class Acceuil extends Component {
   getBackgroundColor(){
     return this.state.backgroundColor
   }
-  
+
   render() {
     return (
       <div className="App">
-        <header className="Acc-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="Acc-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
 
     );
--- mn-app/src/js/header.js
+++ mn-app/src/js/header.js
@@ -4,27 +4,12 @@ import logo from './../img/logo.svg';
 class Acceuil extends Component {
   render() {
     return (
-      <nav class="navbar navbar-default Floatingheader">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <a class="navbar-brand" href="#">
-              <img src={logo} className="App-logo" alt="logo" />
-              <h1 className="App-title">M&Ns Informatique</h1>
-            </a>
-          </div>
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <form class="navbar-form navbar-right">
-              <div class="inline-form">
-                  <input name="login" type="text"/>
-                  <input name="mdp" type="password"/>
-              </div>
-              <button type="submit" class="btn btn-default">Submit</button>
-            </form>
-          </div>
+      <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">M&Ns </a>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <div class="col-md-6"><span class="pull-right">$42</span></div>
         </div>
-      </nav>
-
-
+    </nav>
     );
   }
 }
