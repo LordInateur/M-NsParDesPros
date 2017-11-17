9117c174d107a1f70439ccd36bf4df2283655864
Killy <goureau.n.a@hotmail.fr>
2017-11-17 17:58:18 +0100

    Upgrading NavBar and bootstrap
    Using bootswatch theme in order to get a full bootstrap css file

--- mn-app/src/js/header.js
+++ mn-app/src/js/header.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import logo from './../img/logo.svg';
+import '../css/header.css'
+import '../css/bootstrap.css'
 
-class Acceuil extends Component {
+class Header extends Component {
   render() {
     return (
       <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">M&Ns </a>
-        <div class="collapse navbar-collapse" id="navbarText">
-          <div class="col-md-6"><span class="pull-right">$42</span></div>
-        </div>
-    </nav>
+  <a class="navbar-brand" href="#"><div class="divBrand">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h2>M&Ns Informatique</h2>
+  </div></a>
+  <div class="collapse navbar-collapse" id="navbarColor03">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search"/>
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
     );
   }
 }
 
-export default Acceuil;
+export default Header;
